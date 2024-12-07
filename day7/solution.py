a913336be7bc4729b465c92fbcf6b681cce9c1f9
Malachy O'Connor <malachyguitar@gmail.com>
2024-12-07 21:43:01 +0000

    No counting required

--- day7/solution.py
+++ day7/solution.py
@@ -10,24 +10,24 @@ def concatenate(a, b):
         a *= 10
     return a + b
 
-def count_possibilities(ls, total=0, i=1):
+def is_possible(ls, total=0, i=1):
     if i >= len(ls):
         return total == ls[0]
     if total > ls[0]:
         return False
     
-    return count_possibilities(ls, total + ls[i], i+1) \
-         + count_possibilities(ls, total * ls[i], i+1) \
-         + count_possibilities(ls, concatenate(total, ls[i]), i+1)
+    return is_possible(ls, total + ls[i], i+1) \
+         or is_possible(ls, total * ls[i], i+1) \
+         or is_possible(ls, concatenate(total, ls[i]), i+1)
 
 solution1, solution2 = 0, 0
 
 for level in levels:
-    if count_possibilities(level) > 0:
+    if is_possible(level) > 0:
         solution2 += level[0]
 
 concatenate = lambda x,y: 1000000000000000000000 # We'll discard any solution that involves concatenate (:
 
 for level in levels:
-    if count_possibilities(level) > 0:
+    if is_possible(level):
         solution1 += level[0]
